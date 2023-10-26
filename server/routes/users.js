91bf25bc1b9b4ba0548968664e85ecced8670121
Bonnie Wang <bonnie.wang.423@gmail.com>
2023-10-27 10:33:20 +1300

    add random number

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,22 @@
 import express from 'express'
 
 import * as db from '../db/db.js'
+import multer from 'multer'
 
 const router = express.Router()
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage })
+
+
 
 router.get('/', async (req, res) => {
   try {
@@ -10,6 +24,12 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
+})
+
+router.get('/id', async(req,res)=>{
+  const randomNumber = Math.floor(Math.random()*10)
+  const id = Number(req.params.randomNumber)
+  console.log(id)
 })
 
 router.get('/recipes', async (req, res) => {
@@ -33,4 +53,23 @@ router.get('/:id/comments', async (req, res) => {
   const comments = await db.getComments(recipeId)
   res.render('partials/viewComments', { comments })
 })
+
+router.get('/recipes/add', (req, res) => {
+  res.render('partials/addRecipe')
+})
+
+// router.post('/recipes/add', upload.single('image'), async (req, res) => {
+//   try {
+//     let image = ''
+//     if (req.file) {
+//       image = `images/${req.file.originalname}`
+//     } else if (req.body.imageURL) {
+//       image = req.body.imageURL
+//     }
+//     const { name, discription, name } = req.body
+    
+//   } catch (err) {
+//     console.error(err)
+//   }
+// })
 export default router
